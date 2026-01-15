ded95da0085453c0407db3ed39130d2b9a3646d3
Renan <renanmerlotti@gmail.com>
2026-01-14 22:03:38 -0300

    Commit inicial: unindo back-end e front-end em um monorepo

---
+++ ems-backend/src/main/java/com/project/ems_backend/controller/EmployeeController.java
@@ -0,0 +1,57 @@
+package com.project.ems_backend.controller;
+
+import com.project.ems_backend.dto.EmployeeDto;
+import com.project.ems_backend.service.EmployeeService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+    //REST API Add Employee
+    @PostMapping
+    public ResponseEntity<EmployeeDto> createEmployee(@RequestBody EmployeeDto employeeDto) {
+        EmployeeDto savedEmployee = employeeService.createEmployee(employeeDto);
+
+        return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
+    }
+
+    //REST API Get Employee
+    @GetMapping("{id}")
+    public ResponseEntity<EmployeeDto> getEmployeeById(@PathVariable("id") Long employeeId) {
+        EmployeeDto employeeDto = employeeService.getEmployeeById(employeeId);
+
+        return ResponseEntity.ok(employeeDto);
+    }
+
+    //REST API Get All Employees
+    @GetMapping
+    public ResponseEntity<List<EmployeeDto>> getAllEmployees() {
+        List<EmployeeDto> employees = employeeService.getAllEmployees();
+        return ResponseEntity.ok(employees);
+    }
+
+    //REST API Update Employee
+    @PutMapping("{id}")
+    public ResponseEntity<EmployeeDto> updateEmployee(@PathVariable("id") Long employeeId,@RequestBody EmployeeDto updatedEmployee) {
+        EmployeeDto employeeDto = employeeService.updateEmployee(employeeId, updatedEmployee);
+
+        return ResponseEntity.ok(employeeDto);
+    }
+
+    //REST API Delete Employee
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteEmployee(@PathVariable("id") Long employeeId) {
+        employeeService.deleteEmployee(employeeId);
+
+        return ResponseEntity.ok("Funcionário deletado com sucesso");
+    }
+}
