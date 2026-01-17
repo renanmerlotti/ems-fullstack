1ddd64dc87cddf5f8796749b93682e5f9ec72886
Renan <renanmerlotti@gmail.com>
2026-01-17 20:16:52 -0300

    Instalando a biblioteca react-router-dom para URLs customizadas e configurando no App.jsx

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -2,13 +2,21 @@ import './App.css'
 import Footer from './components/Footer'
 import HeaderComponent from './components/HeaderComponent'
 import ListEmployeeComponent from './components/ListEmployeeComponent'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className='flex flex-col min-h-screen'>
-      <HeaderComponent />
-      <ListEmployeeComponent />
-      <Footer/>
+      <BrowserRouter>
+        <HeaderComponent />
+          <Routes>
+              {/* // http://localhost:3000 */}
+              <Route path='/' element={<ListEmployeeComponent />}></Route>
+              {/* // http://localhost:3000/employees */}
+              <Route path='/employees' element = {<ListEmployeeComponent />}></Route>
+          </Routes>
+        <Footer/>
+      </BrowserRouter>
     </div>
   )
 }
