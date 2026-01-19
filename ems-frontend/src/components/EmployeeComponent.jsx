1bbe40dd726dc41c1ba8e74de12d151ecf3a37f9
Renan <renanmerlotti@gmail.com>
2026-01-18 23:38:45 -0300

    Conectando o axios para fazer o metodo post na API REST e configurando no EmployeeComponent

--- ems-frontend/src/components/EmployeeComponent.jsx
+++ ems-frontend/src/components/EmployeeComponent.jsx
@@ -1,10 +1,15 @@
 import React, { useState } from 'react'
+import { createEmployee } from '../services/EmployeeService'
+import { useNavigate } from 'react-router-dom'
 
 function EmployeeComponent() {
 
   const [firstName, setFirstName] = useState('')
   const [lastName, setLastName] = useState('')
   const [email, setEmail] = useState('')
+  const [age, setAge] = useState('')
+
+  const navigator = useNavigate()
 
   const handleFirstName = (e) => {
     setFirstName(e.target.value)
@@ -18,15 +23,31 @@ function EmployeeComponent() {
     setEmail(e.target.value)
   }
 
+  const handleAge = (e) => {
+    setAge(e.target.value)
+  }
+
   function saveEmployee (e) {
     e.preventDefault()
 
-    const employee = {firstName, lastName, email}
+    const ageInt = parseInt(age)
+
+    const employee = {
+      firstName,
+      lastName,
+      email,
+      age: ageInt
+    }
     console.log(employee)
+
+    createEmployee(employee).then((response) => {
+      console.log(response.data)
+      navigator('/employees')
+    })
   }
 
   return (
-    <div className='container grow mx-auto px-30 py-36 flex justify-center'>
+    <div className='container grow mx-auto px-30 py-30 flex justify-center'>
       <div className='w-full max-w-md rounded-2xl bg-gray-900'>
         <h2 className='text-white p-8 text-2xl text-center font-semibold'>
           Adicionar Funcionário
@@ -69,8 +90,20 @@ function EmployeeComponent() {
             />
           </div>
 
+          <div className='flex flex-col gap-1.5'>
+            <label htmlFor="" className='text-white text-xl ml-1'>Idade do Funcionário</label>
+            <input 
+            type="number"
+            placeholder='Digite a idade do funcionário' 
+            name='age'
+            value={age}
+            className='bg-white rounded-2xl p-2.5 border-none'
+            onChange={handleAge}
+            />
+          </div>
+
           <button 
-          className='w-full mt-8 bg-blue-600 text-white p-3 rounded-2xl font-semibold hover:bg-blue-700 transition active:scale-96 duration-150'
+          className='w-full bg-blue-600 text-white p-3 rounded-2xl font-semibold hover:bg-blue-700 transition active:scale-96 duration-150'
           onClick={saveEmployee}
           >
             Adicionar Funcionario
